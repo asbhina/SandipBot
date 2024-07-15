fd2534727be3ca50d0a682b2cd0cf04adf9894f4
Eroiix-xyz <asmitadk31@gmail.com>
2024-07-15 18:58:02 +0545

    Update profile.js

--- scripts/cmds/profile.js
+++ scripts/cmds/profile.js
@@ -1,8 +1,7 @@
 module.exports = {
 	config: {
 		name: "profile",
-aliases: ["pfp"],
-
+		aliases: ["pfp"],
 		version: "1.1",
 		author: "NIB",
 		countDown: 5,
@@ -11,7 +10,7 @@ aliases: ["pfp"],
 		longDescription: "PROFILE image",
 		category: "media",
 		guide: {
-			en: "   {pn} @tag"
+			en: "{pn} @tag"
 		}
 	},
 
@@ -25,19 +24,23 @@ aliases: ["pfp"],
 	},
 
 	onStart: async function ({ event, message, usersData, args, getLang }) {
-    let avt;
+		let avt;
 		const uid1 = event.senderID;
 		const uid2 = Object.keys(event.mentions)[0];
-		if(event.type == "message_reply"){
-      avt = await usersData.getAvatarUrl(event.messageReply.senderID)
-    } else{
-      if (!uid2){avt =  await usersData.getAvatarUrl(uid1)
-              } else{avt = await usersData.getAvatarUrl(uid2)}}
-
+		
+		if (event.type === "message_reply") {
+			avt = await usersData.getAvatarUrl(event.messageReply.senderID);
+		} else {
+			if (!uid2) {
+				avt = await usersData.getAvatarUrl(uid1);
+			} else {
+				avt = await usersData.getAvatarUrl(uid2);
+			}
+		}
 
 		message.reply({
-			body:"",
+			body: "",
 			attachment: await global.utils.getStreamFromURL(avt)
-	})
-  }
+		});
+	}
 };
