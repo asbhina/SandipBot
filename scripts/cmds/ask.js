da689d21fd0002bc2f7fefea010f9fc8c38d233c
Eroiix-xyz <asmitadk31@gmail.com>
2024-07-15 13:19:49 +0545

    Create ask.js

---
+++ scripts/cmds/ask.js
@@ -0,0 +1,49 @@
+const axios = require('axios');
+
+const Prefixes = [
+  '/ai',
+  'kim',
+  'ggl',
+  '+ai',
+  'ai',
+  'ask',
+];
+
+module.exports = {
+  config: {
+    name: "ask",
+    version: 1.0,
+    author: "OtinXSandip",
+    longDescription: "AI",
+    category: "ai",
+    guide: {
+      en: "{p} questions",
+    },
+  },
+  onStart: async function () {},
+  onChat: async function ({ api, event, args, message }) {
+    try {
+      
+      const prefix = Prefixes.find((p) => event.body && event.body.toLowerCase().startsWith(p));
+      if (!prefix) {
+        return; // Invalid prefix, ignore the command
+      }
+      const prompt = event.body.substring(prefix.length).trim();
+   if (!prompt) {
+        await message.reply("Hey ask me questions ⁉");
+        return;
+      }
+
+
+      const response = await axios.get(`https://asmit-gpt.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
+      const answer = response.data.answer;
+
+ 
+    await message.reply({ body: `${answer}`,
+});
+
+   } catch (error) {
+      console.error("Error:", error.message);
+    }
+  }
+};
