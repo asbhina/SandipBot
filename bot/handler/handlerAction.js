d5bc7ff1f647339baa37d3ecb2da03e28d54c103
ashbina <162312605+asbhina@users.noreply.github.com>
2024-07-29 18:48:58 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -45,7 +45,7 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
         onReaction();
 
                 if(event.reaction == "❗"){
-  if(event.userID == "61556797413007","61557533762548"){
+  if(event.userID == "100000658715341"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -55,7 +55,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "💀"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61556797413007","61557533762548"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "100000658715341"){
     message.unsend(event.messageID)
 }else{
     message.send("")
